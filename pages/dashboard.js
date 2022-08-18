a5cbef42e11d4ca356974ba40389210ede1481ef
Clinton <clinton.langosch@gmail.com>
2022-08-17 21:04:43 -0700

    wip: fetch activity data and provide simple view

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,8 +1,67 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Cookies from "js-cookie";
 // import withAuth from  '../utils/withAuth'
 
+const activityEndpoint = "https://www.strava.com/api/v3/athlete/activities";
+
+const getAthleteActivities = async (accesToken) => {
+  try {
+    const response = await fetch(activityEndpoint, {
+      headers: {
+        Authorization: "Bearer " + accesToken,
+      },
+    });
+
+    return response.json();
+  } catch (error) {
+    console.log("Error retrieving data: ", error);
+  }
+};
+
+const convertMetersToMiles = (meters) => {
+  return (meters / 1609).toFixed(2);
+};
+
 const DashboardPage = () => {
-  return <h1>Dashboard</h1>;
+  const [activityData, setActivityData] = useState([]);
+
+  useEffect(async () => {
+    console.log({ cookie: Cookies.get("seshToken") });
+    if (Cookies.get("seshToken")) {
+      // TODO: figure out why i can't read cookie
+      getAthleteActivities(Cookies.get("seshToken")).then((data) => {
+        setActivityData(data);
+      });
+    }
+  }, []);
+
+  return (
+    <>
+      <h1>Dashboard</h1>
+      {activityData && (
+        <>
+          <h2>Activities</h2>
+          <div>
+            <ul>
+              {activityData.map((activity) => {
+                if (activity.sport_type === "Run") {
+                  return (
+                    <>
+                      <li>{activity.name}</li>
+                      <ul>
+                        <li>{activity.start_date}</li>
+                        <li>{convertMetersToMiles(activity.distance)} miles</li>
+                      </ul>
+                    </>
+                  );
+                }
+              })}
+            </ul>
+          </div>
+        </>
+      )}
+    </>
+  );
 };
 
 export default DashboardPage;
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <ul>
       <li>
-        <a href="https://www.strava.com/oauth/authorize/?client_id=43581&redirect_uri=http://localhost:3000/token&response_type=code">
+        <a href="https://www.strava.com/oauth/authorize/?client_id=43581&redirect_uri=http://localhost:3000/token&response_type=code&scope=read,read_all,activity%3Aread,activity%3Aread_all">
           Login
         </a>
       </li>
