279d0e750ba3f64b2cb9e9c1c34a84eae858b9c8
Clinton <clinton.langosch@gmail.com>
2022-08-18 17:58:24 -0700

    fix: update domain via env vars

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 export default function Home() {
+  const domain = process.env.DOMAIN || "localhost";
+  const port = process.env.PORT ? `:${process.env.PORT}` : `:3000`;
   return (
     <ul>
       <li>
-        <a href="https://www.strava.com/oauth/authorize/?client_id=43581&redirect_uri=http://localhost:3000/token&response_type=code&scope=read,read_all,activity%3Aread,activity%3Aread_all">
+        <a
+          href={`https://www.strava.com/oauth/authorize/?client_id=43581&redirect_uri=http://${domain}${port}/token&response_type=code&scope=read,read_all,activity%3Aread,activity%3Aread_all`}
+        >
           Login
         </a>
       </li>
