21550a7fa3212725e27ea99205bfa23300c834e8
Clinton <clinton.langosch@gmail.com>
2023-01-17 21:17:09 -0800

    wip: add goal json and placeholder calendar component

---
+++ components/Calendar.js
@@ -0,0 +1,12 @@
+//TODO: Either write my own based on https://programmingwithmosh.com/react/build-a-react-calendar-component-from-scratch/
+
+// OR 
+
+// use react-calendar https://github.com/wojtekmaj/react-calendar
+const Calendar = ({startDate, endDate}) => {
+  return (
+    
+  )
+}
+
+export default Calendar;
